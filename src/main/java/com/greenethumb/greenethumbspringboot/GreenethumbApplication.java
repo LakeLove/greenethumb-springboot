87bad7ac611ad3770b1fa0476d432b11d8ffdf1a
lakegreene <lakegreene@yahoo.com>
2020-09-10 14:18:37 -0400

    Initial commit

---
+++ src/main/java/com/greenethumb/greenethumbspringboot/GreenethumbApplication.java
@@ -0,0 +1,13 @@
+package com.greenethumb.greenethumbspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenethumbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenethumbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenethumb/greenethumbspringboot/GreenethumbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenethumb.greenethumbspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenethumbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
